f5ed17df7fc0f7cc7408475b6da45244beb1f967
dtannyc1 <dtannyc1@gmail.com>
2023-05-26 11:48:43 -0400

    set up cookies, jbuilder, gems, rails port

--- config/application.rb
+++ config/application.rb
@@ -34,6 +34,13 @@ module Davescord
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
+
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore,
+      key: '_auth_me_session',
+      same_site: :lax,
+      secure: Rails.env.production?
+
     config.api_only = true
   end
 end
