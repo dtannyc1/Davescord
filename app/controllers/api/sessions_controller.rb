396aa500500a6ab4e53870e9e6c4a0b89616c0cc
dtannyc1 <dtannyc1@gmail.com>
2023-05-26 16:22:59 -0400

    wrote SessionsController functions

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,10 +1,26 @@
 class Api::SessionsController < ApplicationController
-  def show
-  end
+    def show
+        if current_user
+            @user = current_user
+            render 'api/users/show'
+        else
+            render json: {user: nil}
+        end
+    end
 
-  def create
-  end
+    def create
+        @user = User.find_by_credentials(params[:credential], params[:password])
 
-  def destroy
-  end
+        if @user
+            login!(@user)
+            render 'api/users/show'
+        else
+            render json: { errors: ['Login or password is invalid']}
+        end
+    end
+
+    def destroy
+        logout! if current_user
+        render json: { message: 'Logout Success'}
+    end
 end
