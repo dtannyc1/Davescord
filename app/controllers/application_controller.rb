506cecae061f06225d11e28eb0c30fb2e3265167
dtannyc1 <dtannyc1@gmail.com>
2023-05-26 16:09:25 -0400

    added basic auth to application_controller, can't test yet b/c of csrf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,36 @@ class ApplicationController < ActionController::API
     before_action :snake_case_params, :attach_authenticity_token
 
     def test
-        render json: {message: ["Hello from Rails"]}
+        if params.has_key?(:login)
+            login!(User.first)
+        elsif params.has_key?(:logout)
+            logout!
+        end
+
+        if current_user
+            render json: { user: current_user.slice('id', 'username', 'session_token') }
+        else
+            render json: ['No current user']
+        end
     end
 
+    def current_user
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
+    def login!(user)
+        session[:session_token] = user.reset_session_token!
+    end
+
+    def logout!
+        current_user.reset_session_token! if (current_user)
+        session[:session_token] = nil
+        @current_user = nil
+    end
+
+    def require_logged_in
+        render(json: { message: "Unauthorized" }, status: :unauthorized) unless current_user
+    end
 
     private
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
     # Defines the root path route ("/")
     # root "articles#index"
 
-    get 'api/test', to: 'application#test'
+    post 'api/test', to: 'application#test'
 
 end
