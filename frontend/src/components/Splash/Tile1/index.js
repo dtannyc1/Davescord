7404fd114f941514cc886e1e627e14892d5fe2a6
dtannyc1 <dtannyc1@gmail.com>
2023-05-29 15:49:26 -0400

    centered navbar

--- frontend/src/components/Splash/Tile1/index.js
+++ frontend/src/components/Splash/Tile1/index.js
@@ -7,12 +7,13 @@ const Tile1 = () => {
     return (
         <div className="tile1-holder">
             <img src={backgroundImg} className="tile1-background1" alt="mountains-background"/>
-            <img src={shoes} className="tile1-shoes" alt="characters-in-shoes"/>
-            <img src={sipping} className="tile1-sipping" alt="characters-sipping-drinks"/>
             <div className="tile1-text">
                 <h3>IMAGINE A PLACE...</h3>
                 <p>...where you can belong to a school club, a gaming group, or a worldwide art community. Where just you and a handful of friends can spend time together. A place that makes it easy to talk every day and hang out more often.</p>
             </div>
+
+            <img src={shoes} className="tile1-shoes" alt="characters-in-shoes"/>
+            <img src={sipping} className="tile1-sipping" alt="characters-sipping-drinks"/>
         </div>
     )
 }
