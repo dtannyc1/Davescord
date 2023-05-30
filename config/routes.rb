373ef381f00b3c9332b5b7f9e6efc620fcd8c66c
dtannyc1 <dtannyc1@gmail.com>
2023-05-30 11:24:43 -0400

    added subscription routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
     namespace :api, defaults: { format: :json } do
         resources :users, only: [:create]
         resources :servers, only: [:index, :show, :create, :update, :destroy]
+        resources :subscriptions, only: [:index, :create, :destroy]
         resource :session, only: [:show, :create, :destroy]
     end
 end
