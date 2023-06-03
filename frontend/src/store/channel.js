166cc053fb345420eb251c67cdb5bbfd5d6189cc
dtannyc1 <dtannyc1@gmail.com>
2023-06-03 15:48:26 -0400

    wrote action creators and additional switch cases in channelsReducer

--- frontend/src/store/channel.js
+++ frontend/src/store/channel.js
@@ -1,6 +1,8 @@
 
 // action types
+const ADD_CHANNEL = 'channels/ADD_CHANNEL';
 const ADD_CHANNELS = 'channels/ADD_CHANNELS';
+const REMOVE_CHANNEL = 'channels/REMOVE_CHANNEL';
 
 // action creators
 export const addChannels = channels => {
@@ -10,13 +12,33 @@ export const addChannels = channels => {
     }
 }
 
+export const addChannel = channel => {
+    return {
+        type: ADD_CHANNEL,
+        channel
+    }
+}
+
+export const removeChannel = channelId => {
+    return {
+        type: REMOVE_CHANNEL,
+        channelId
+    }
+}
+
 // thunk action creators
 
 // reducer
 const channelsReducer = (state = {}, action) => {
+    let nextState = {...state}
     switch (action.type) {
+        case ADD_CHANNEL:
+            return {...nextState, ...action.channel}
         case ADD_CHANNELS:
             return {...action.channels}
+        case REMOVE_CHANNEL:
+            delete nextState[action.channelId]
+            return nextState
         default:
             return state
     }
