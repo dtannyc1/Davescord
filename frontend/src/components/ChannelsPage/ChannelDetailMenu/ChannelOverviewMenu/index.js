ff66e9db19ff6a5a9d09658dff9d5e2a7c9d97c8
dtannyc1 <dtannyc1@gmail.com>
2023-06-04 13:45:30 -0400

    finished update and delete servers

--- frontend/src/components/ChannelsPage/ChannelDetailMenu/ChannelOverviewMenu/index.js
+++ frontend/src/components/ChannelsPage/ChannelDetailMenu/ChannelOverviewMenu/index.js
@@ -9,22 +9,36 @@ const ChannelMenu = ({visibility, visibilitySetter}) => {
     const {channelId} = useParams();
     const currentChannel = useSelector(state => state.channels[channelId]);
     let originalChannelName = currentChannel ? currentChannel.channelName : '';
+    let originalChannelCategory = currentChannel ? currentChannel.categoryName : '';
+    let originalChannelTopicName = currentChannel ? currentChannel.description : '';
     const [channelName, setChannelName] = useState(originalChannelName);
+    const [category, setCategory] = useState(originalChannelCategory);
+    const [description, setDescription] = useState(originalChannelTopicName);
 
     useEffect(() => {
         originalChannelName =  currentChannel ? currentChannel.channelName : '';
-        setChannelName(originalChannelName)
+        originalChannelCategory = currentChannel ? currentChannel.categoryName : '';
+        originalChannelTopicName = currentChannel ? currentChannel.description : '';
+        resetFields();
     }, [currentChannel])
 
     useEffect(() => {
         if (!visibility){
-            setChannelName(originalChannelName);
+            resetFields();
         }
     }, [visibility])
+
+    const resetFields = () => {
+        setChannelName(originalChannelName);
+        setCategory(originalChannelCategory);
+        setDescription(originalChannelTopicName);
+    }
 
     const changeChannel = e => {
         e.preventDefault();
         currentChannel.channelName = channelName;
+        currentChannel.categoryName = category;
+        currentChannel.description = description;
         dispatch(updateChannel(currentChannel));
         visibilitySetter(false);
     }
@@ -38,11 +52,24 @@ const ChannelMenu = ({visibility, visibilitySetter}) => {
                     <form>
                         <input className='channel-overview-input' type='text' value={channelName} onChange={e => setChannelName(e.target.value)}/>
                     </form>
+
+                    <span className='channel-overview-input-title'>category</span>
+                    <form>
+                        <input className='channel-overview-input' type='text' value={category} onChange={e => setCategory(e.target.value)}/>
+                    </form>
+
+                    <span className='channel-overview-input-title'>channel topic</span>
+                    <form>
+                        <input className='channel-overview-input' type='text' value={description} onChange={e => setDescription(e.target.value)}/>
+                    </form>
                 </div>
-                <div className={(originalChannelName === channelName) ? 'save-button-holder hidden' : 'save-button-holder'}>
+                <div className={(originalChannelName !== channelName ||
+                                 originalChannelCategory !== category ||
+                                 originalChannelTopicName !== description )
+                        ? 'save-button-holder' : 'save-button-holder hidden'}>
                     <div>Careful - you have unsaved changes!</div>
                     <div>
-                        <button className='channel-overview-reset-button' onClick={e => setChannelName(originalChannelName)}>Reset</button>
+                        <button className='channel-overview-reset-button' onClick={e => resetFields()}>Reset</button>
                         <button className='channel-overview-save-button' onClick={changeChannel}>Save Changes</button>
                     </div>
                 </div>
