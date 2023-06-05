7488b25f1ec36b821b2e4e6aaedbab0b7c309643
dtannyc1 <dtannyc1@gmail.com>
2023-06-04 21:48:36 -0400

    wrote and tested create update and destroy message controller functions

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,21 +1,52 @@
 class Api::MessagesController < ApplicationController
 
-    def index
+    def index # '/api/channels/1/messages'
         @messages = Message.where(channel_id: params[:channel_id])
 
         render :index
     end
 
-    def create
+    def create # '/api/channels/1/messages', 'POST'
+        @message = Message.new(message_params)
+        @message.channel_id = params[:channel_id]
+        @message.author_id = current_user.id
 
+        if (@message.save)
+            render :show
+        else
+            render json: {errors: @message.errors.full_messages}, status: 422
+        end
     end
 
-    def update
+    def update # '/api/messages/1', 'PUT'
+        @message = Message.find(params[:id])
 
+        if (@message)
+            if (@message.author_id == current_user.id)
+                if @message.update(message_params)
+                    render :show
+                end
+            else
+                render json: {errors: 'Unauthorized, must be author to edit message'}, status: :unauthorized
+            end
+        else
+            render json: {errors: "Message not found"}, status: 404
+        end
     end
 
-    def destroy
-
+    def destroy # '/api/messages/1', 'DELETE'
+        @message = Message.find_by!(id: params[:id])
+        if (@message)
+            server_owner = @message.channel.server.owner
+            if (@message.author_id == current_user.id || server_owner.id == current_user.id)
+                @message.destroy
+                render json: nil
+            else
+                render json: {errors: 'Unauthorized, must be author or server owner to delete message'}, status: :unauthorized
+            end
+        else
+            render json: {errors: "Message not found"}, status: 404
+        end
     end
 
     private
---
+++ app/views/api/messages/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @message, :author_id, :body, :created_at
