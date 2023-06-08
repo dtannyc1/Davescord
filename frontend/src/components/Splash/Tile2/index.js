9f9498a7c8a95107adda56b62e0f55e30c8ee2f6
dtannyc1 <dtannyc1@gmail.com>
2023-06-08 11:22:55 -0400

    fixed splash tile 2 resize issue

--- frontend/src/components/Splash/Tile2/index.js
+++ frontend/src/components/Splash/Tile2/index.js
@@ -4,7 +4,7 @@ import channels from '../../../assets/channels.svg';
 const Tile2 = () => {
     return (
         <div className="tile2-holder">
-            <img src={channels} alt="davescord channel demo"/>
+            <img className="tile2-image" src={channels} alt="davescord channel demo"/>
             <div className="tile2-text">
                 <h3>Create an invite-only place where you belong</h3>
                 <p>Discord servers are organized into topic-based channels where you can collaborate, share, and just talk about your day without clogging up a group chat.</p>
