529d20572c392bc89015da53fe138135a32ce6f4
dtannyc1 <dtannyc1@gmail.com>
2023-06-08 12:31:56 -0400

    fixed server detail menu resize issues

--- frontend/src/components/ServerDetailsMenu/OverviewMenu/index.js
+++ frontend/src/components/ServerDetailsMenu/OverviewMenu/index.js
@@ -34,17 +34,15 @@ const OverviewMenu = ({visibility, visibilitySetter}) => {
             <h3>Server Overview</h3>
             <div>
                 <div>
-                    {/* change server picture */}
-                </div>
-                <div>
                     <span className='overview-input-title'>server name</span>
                     <form>
                         <input className='overview-input' type='text' value={serverName} onChange={e => setServerName(e.target.value)}/>
                     </form>
                 </div>
-                <div className={(originalServerName.current === serverName) ? 'save-button-holder hidden' : 'save-button-holder'}>
-                    <div>Careful - you have unsaved changes!</div>
-                    <div>
+                <div className={(originalServerName.current === serverName) ?
+                        'save-button-holder hidden' : 'save-button-holder'}>
+                    <div className="save-button-text">Careful - you have unsaved changes!</div>
+                    <div className="buttons">
                         <button className='overview-reset-button' onClick={e => setServerName(originalServerName.current)}>Reset</button>
                         <button className='overview-save-button' onClick={changeServer}>Save Changes</button>
                     </div>
