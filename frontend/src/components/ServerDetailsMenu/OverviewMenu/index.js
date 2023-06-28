7cbef727f20cc8b7a851be899e58c8a8ffcfbf3b
dtannyc1 <dtannyc1@gmail.com>
2023-06-28 17:18:52 -0400

    start add photo update for servers

--- frontend/src/components/ServerDetailsMenu/OverviewMenu/index.js
+++ frontend/src/components/ServerDetailsMenu/OverviewMenu/index.js
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import './OverviewMenu.css'
 import { useEffect, useRef, useState } from 'react';
 import { updateServer } from '../../../store/server';
+import image_upload from '../../../assets/create_server_image_upload.svg';
 
 const OverviewMenu = ({visibility, visibilitySetter}) => {
     const dispatch = useDispatch();
@@ -10,6 +11,8 @@ const OverviewMenu = ({visibility, visibilitySetter}) => {
     const currentServer = useSelector(state => state.servers[serverId]);
     let originalServerName = useRef(currentServer ? currentServer.serverName : '');
     const [serverName, setServerName] = useState(originalServerName.current);
+    const [photoFile, setPhotoFile] = useState (null);
+    const validImageTypes = ['image/gif', 'image/jpeg', 'image/png'];
 
     useEffect(() => {
         originalServerName.current =  currentServer ? currentServer.serverName : '';
@@ -29,15 +32,36 @@ const OverviewMenu = ({visibility, visibilitySetter}) => {
         visibilitySetter(false);
     }
 
+    const handleFile = ({ currentTarget }) => {
+        const file = currentTarget.files[0];
+        if (file && validImageTypes.includes(file['type'])){
+            setPhotoFile(file);
+        } else {
+            setPhotoFile(null);
+        }
+    }
+
     return (
         <div className="overview-menu">
             <h3>Server Overview</h3>
             <div>
-                <div>
-                    <span className='overview-input-title'>server name</span>
-                    <form>
-                        <input className='overview-input' type='text' value={serverName} onChange={e => setServerName(e.target.value)}/>
-                    </form>
+                <div className="flex-row">
+                    <div className="left-option">
+                        <label htmlFor="file-input">
+                            {photoFile ? <img className="server-image" src={URL.createObjectURL(photoFile)} alt="upload"/>
+                                : <img className="server-image-upload" src={image_upload} alt="upload"/>}
+                        </label>
+                        <input type="file" id="file-input" name="file" accept="image/*" onChange={handleFile} />
+                        <div>
+                            We recommend an image of at least 512x512 for the server.
+                        </div>
+                    </div>
+                    <div>
+                        <span className='overview-input-title'>server name</span>
+                        <form>
+                            <input className='overview-input' type='text' value={serverName} onChange={e => setServerName(e.target.value)}/>
+                        </form>
+                    </div>
                 </div>
                 <div className={(originalServerName.current === serverName || serverName.length === 0) ?
                         'save-button-holder hidden' : 'save-button-holder'}>
