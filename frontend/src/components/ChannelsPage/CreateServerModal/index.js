a0df5a90e8d8f06023ab41d7d292980975ccdcc9
dtannyc1 <dtannyc1@gmail.com>
2023-06-30 11:18:49 -0400

    fix error with server creation

--- frontend/src/components/ChannelsPage/CreateServerModal/index.js
+++ frontend/src/components/ChannelsPage/CreateServerModal/index.js
@@ -33,18 +33,19 @@ const CreateServerModal = ({visible, setVisible, setWebsocketRestart}) => {
             let newServer = await dispatch(createServer(formData))
             let channel = {
                 channel_name: "general",
-                serverId: newServer.id,
+                serverId: newServer.server.id,
                 categoryName: "Text Channels",
                 description: ""
             }
 
+            debugger
             setNewServerName(defaultServerName.current);
             setPhotoFile(null);
             setVisible(false)
             let newChannel = await dispatch(createChannel(channel))
             dispatch(addChannel(newChannel))
 
-            history.push(`/channels/${newServer.id}/${newChannel.id}`)
+            history.push(`/channels/${newServer.server.id}/${newChannel.id}`)
 
             setWebsocketRestart(!websocketRestart) // force restart websockets
         }
