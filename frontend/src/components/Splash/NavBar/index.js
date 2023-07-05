6fc34b64b158a3a82b18216f523ac6773d1d6655
dtannyc1 <dtannyc1@gmail.com>
2023-07-05 10:15:43 -0400

    add icons to navbar

--- frontend/src/components/Splash/NavBar/index.js
+++ frontend/src/components/Splash/NavBar/index.js
@@ -11,9 +11,20 @@ const NavBar = () => {
                 <MainLogo/>
                 <div className="navbar-links">
                     <Link to={{pathname: "https://github.com/dtannyc1/Davescord"}}
-                        target="_blank">Github</Link>
+                        target="_blank">
+                            <i className="fa-brands fa-github fa-xl"></i>
+                            <div>Github</div>
+                    </Link>
                     <Link to={{pathname: "https://www.linkedin.com/in/dtannyc1/"}}
-                        target="_blank">LinkedIn</Link>
+                        target="_blank">
+                            <i className="fa-brands fa-linkedin fa-xl"></i>
+                            <div>LinkedIn</div>
+                    </Link>
+                    <Link to={{pathname: "https://wellfound.com/u/david-tan-47"}}
+                        target="_blank">
+                            <i className="fa-brands fa-angellist fa-xl"></i>
+                            <div>Wellfound</div>
+                    </Link>
                 </div>
                 <Link to="/login" className="navbar-login">Login</Link>
             </div>
