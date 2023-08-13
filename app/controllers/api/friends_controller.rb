f1e70e8d41a06bf18d79d09c78a8254687b19d2e
dtannyc1 <dtannyc1@gmail.com>
2023-08-13 12:32:39 -0400

    add comments to friends controller

--- app/controllers/api/friends_controller.rb
+++ app/controllers/api/friends_controller.rb
@@ -1,2 +1,29 @@
 class Api::FriendsController < ApplicationController
+    def index
+        # get list of all friends for current user
+        # all friender and friendee options
+    end
+
+    def create
+        # create new friend request
+        # default to pending
+
+    end
+
+    def update
+        # update existing friend request
+        # either to denied or approved
+
+    end
+
+    def destroy
+        # unfriend
+
+    end
+
+    private
+    def friends_params
+        params.require(:friend).permit(:friendee_id)
+        # friender_id is logged in user, status defaults to pending
+    end
 end
