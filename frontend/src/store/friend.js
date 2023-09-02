1a6d631112785ff37f3fbd4a6d76ba80debcc00a
dtannyc1 <dtannyc1@gmail.com>
2023-09-02 12:02:18 -0400

    add friend action types and creators, update friends reducer

--- frontend/src/store/friend.js
+++ frontend/src/store/friend.js
@@ -1,8 +1,31 @@
 import csrfFetch from "./csrf";
 
 // action types
+const ADD_FRIEND = 'friends/ADD_FRIEND';
+const ADD_FRIENDS = 'friends/ADD_FRIENDS';
+const REMOVE_FRIEND = 'friends/REMOVE_FRIEND';
 
 // action creators
+export const addFriend = friend => {
+    return {
+        type: ADD_FRIEND,
+        friend
+    }
+}
+
+export const addFriends = friends => {
+    return {
+        type: ADD_FRIENDS,
+        friends
+    }
+}
+
+export const removeFriend = friendId => {
+    return {
+        type: REMOVE_FRIEND,
+        friendId
+    }
+}
 
 // thunk action creators
 
@@ -12,6 +35,14 @@ const friendsReducer = (state = {}, action) => {
     let nextState = {...state};
 
     switch (action.type){
+        case ADD_FRIEND:
+            nextState[action.friend.id] = action.friend;
+            return nextState;
+        case ADD_FRIENDS:
+            return {...action.friends};
+        case REMOVE_FRIEND:
+            delete nextState[action.friendId]
+            return nextState;
         default:
             return state;
     }
