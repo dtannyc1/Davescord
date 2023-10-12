162d09c7d0f8992a5312b7fc371bf774808e92a5
dtannyc1 <dtannyc1@gmail.com>
2023-10-12 12:55:17 -0400

    create private chats controller methods without Websockets

--- app/controllers/api/privatechats_controller.rb
+++ app/controllers/api/privatechats_controller.rb
@@ -8,19 +8,36 @@ class Api::PrivatechatsController < ApplicationController
     end
 
     def create
+        params[:private_chat][:user_1_id] = current_user.id
+        @private_chat = PrivateChat.new(private_chat_params)
 
-    end
+        if @private_chat.save
 
-    def update
+            # Need to broadcast websocket here
 
+            render :show
+        else
+            render json: @private_chat.errors.full_messages, status: 422
+        end
     end
 
     def destroy
+        @private_chat = PrivateChat.find(params[:id])
 
+        if (@private_chat.user_1_id == current_user.id ||
+            @private_chat.user_2_id == current_user.id)
+
+            # Need to broadcast websocket here
+
+            @private_chat.destroy
+            render json: nil
+        else
+            render json: {errors: 'Unauthorized, must be owner to delete private chat'}, status: :unauthorized
+        end
     end
 
     private
     def private_chat_params
-        params.require(:private_chat).permit(:user_1_id, :user_2_id)
+        params.require(:private_chat).permit(:user_2_id)
     end
 end
