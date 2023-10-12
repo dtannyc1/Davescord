736b15d48bcf6b5bb1c50ec5e23a701937ac99eb
dtannyc1 <dtannyc1@gmail.com>
2023-10-12 13:28:31 -0400

    create private message methods

--- app/controllers/api/privatemessages_controller.rb
+++ app/controllers/api/privatemessages_controller.rb
@@ -8,11 +8,35 @@ class Api::PrivatemessagesController < ApplicationController
     end
 
     def create
+        @private_message = PrivateMessage.new(private_messages_params)
+        @private_message.author_id = current_user.id
+
+        if (@private_message.save)
+
+            # Broadcast WebSocket here
+            render :show
+        else
+            render json: {errors: @private_message.errors.full_messages}, status: 422
+        end
 
     end
 
     def update
+        @private_message = PrivateMessage.find(params[:id])
 
+        if (@private_message)
+            if (@private_message.author_id == current_user.id)
+                if (@private_message.update(private_messages_params))
+                    # Broadcast WebSocket here
+
+                    render :show
+                end
+            else
+                render json: {errors: "Unauthorized, must be author to edit private message"}, status: :unauthorized
+            end
+        else
+            render json: {errors: "Private message not found"}, status: 404
+        end
     end
 
     def destroy
@@ -21,6 +45,6 @@ class Api::PrivatemessagesController < ApplicationController
 
     private
     def private_messages_params
-        params.require(:private_messages).permit(:private_chat_id)
+        params.require(:private_messages).permit(:private_chat_id, :body)
     end
 end
