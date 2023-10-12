af96e4c6b77075e0a8add495a1dc0b030b8abcf8
dtannyc1 <dtannyc1@gmail.com>
2023-10-12 11:08:00 -0400

    setup methods for private chats controller

--- app/controllers/api/privatechats_controller.rb
+++ app/controllers/api/privatechats_controller.rb
@@ -1,2 +1,24 @@
 class Api::PrivatechatsController < ApplicationController
+    before_action(:require_logged_in, only: [:index, :show, :create, :update, :destroy])
+
+    def index
+
+    end
+
+    def create
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+    private
+    def private_chat_params
+        params.require(:private_chat).permit(:user_1_id, :user_2_id)
+    end
 end
