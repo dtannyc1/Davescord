9f428c7bf96a22866da29808fd5d0da92e2972c1
dtannyc1 <dtannyc1@gmail.com>
2023-10-12 17:38:44 -0400

    add redirect to correct private chat on click

--- frontend/src/components/ChannelsPage/PrivateChatsList/PrivateChatItem/index.js
+++ frontend/src/components/ChannelsPage/PrivateChatsList/PrivateChatItem/index.js
@@ -3,9 +3,12 @@ import './PrivateChatItem.css'
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { fetchUser } from '../../../../store/user';
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const PrivateChatItem = ({privateChat}) => {
     const dispatch = useDispatch();
+    const history = useHistory();
+    const {channelId} = useParams();
     const users = useSelector(state => state.users);
     const currentUserId = useSelector(state => state.session.currentUserId);
     const [otherUserId, setOtherUserId] = useState(null);
@@ -24,10 +27,17 @@ const PrivateChatItem = ({privateChat}) => {
         }
     }, [dispatch, privateChat])
 
+    let handleClick = function(e){
+        e.stopPropagation();
+        if (parseInt(privateChat.id) !== channelId){
+            history.push(`/channels/@me/${parseInt(privateChat.id)}`)
+        }
+    }
+
     return (
         <>
         {users[otherUserId] ?
-            <div key={users[otherUserId]?.id} className='private-chat-item'>
+            <div key={users[otherUserId]?.id} className='private-chat-item' onClick={e => handleClick(e)}>
                 {(users[otherUserId]?.photoUrl) ?
                     <img className='user-item-img' src={users[otherUserId]?.photoUrl} alt={users[otherUserId]?.username.toUpperCase().charAt(0)}/> :
                     ((users[otherUserId]?.color) ?
