b77a2fa03cd7e8e4d48105818c722bc953887f38
dtannyc1 <dtannyc1@gmail.com>
2023-10-12 13:15:10 -0400

    create private messages index controller and view

--- app/controllers/api/privatemessages_controller.rb
+++ app/controllers/api/privatemessages_controller.rb
@@ -1,2 +1,26 @@
 class Api::PrivatemessagesController < ApplicationController
+    before_action(:require_logged_in, only: [:index, :show, :create, :update, :destroy])
+
+    def index
+        @private_chat = PrivateChat.find(params[:private_chat_id])
+
+        render :index
+    end
+
+    def create
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+    private
+    def private_messages_params
+        params.require(:private_messages).permit(:private_chat_id)
+    end
 end
--- app/views/api/privatechats/show.json.jbuilder
+++ app/views/api/privatechats/show.json.jbuilder
@@ -3,11 +3,3 @@ json.private_chats do
         json.extract @private_chat, :id, :user_1_id, :user_2_id
     end
 end
-
-json.private_messages do
-    @private_chat.private_messages.each do |message|
-        json.set! message.id do
-            json.extract message, :id, :author_id, :private_chat_id, :body, :created_at, :updated_at
-        end
-    end
-end
